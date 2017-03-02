4921728831e08e5071caced263fa7439ee81a6bc
Rein Chow <rein.chow@firstidea.vn>
2017-03-02 10:53:31 +0700

    Added @injectable to RepositoryBase.

--- dist/app/persistence/RepositoryBase.js
+++ dist/app/persistence/RepositoryBase.js
@@ -1,4 +1,13 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -7,8 +16,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const DependencyContainer_1 = require("../utils/DependencyContainer");
 const Guard_1 = require("../utils/Guard");
-class RepositoryBase {
+let RepositoryBase = class RepositoryBase {
     constructor(_modelMapper) {
         this._modelMapper = _modelMapper;
         Guard_1.Guard.assertDefined('modelMapper', this._modelMapper);
@@ -60,7 +70,11 @@ class RepositoryBase {
             return affectedRows;
         });
     }
-}
+};
+RepositoryBase = __decorate([
+    DependencyContainer_1.injectable(),
+    __metadata("design:paramtypes", [Object])
+], RepositoryBase);
 exports.RepositoryBase = RepositoryBase;
 
 //# sourceMappingURL=RepositoryBase.js.map
